282802383944fd7e17f7271cfa37252fc8561918
Bhupesh Rajendra Chaudhari <141324060+BhupeshChaudhari@users.noreply.github.com>
2024-03-15 03:24:07 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,24 @@
+// let score = "19abc"
+let score = undefined
+// let score = null
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+// null => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//
