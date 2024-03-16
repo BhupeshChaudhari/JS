2850f898ea3361c05d4c7b40125803a5dc83e83e
Bhupesh Rajendra Chaudhari <141324060+BhupeshChaudhari@users.noreply.github.com>
2024-03-16 03:52:20 +0000

    datatypes

---
+++ 01_basics/DataTypesSummary.js
@@ -0,0 +1,15 @@
+
+// 1. Primitive Datatype (Call by value --> Copy of data does not provide actual ref)
+
+//     String  -> string
+//     number  -> number
+//     boolean -> boolean
+//     null -> Object
+//     undefined -> undefined
+//     symbol -> symbol
+
+// 2. Non- Primitive (Reference Type) (call by reference --> actual ref provided)
+
+//     Array -> Object
+//     Object -> Object
+//     Functions -> Function
