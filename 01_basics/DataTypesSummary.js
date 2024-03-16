de2487b8706840ebffe3968369d138d3f6681c87
Bhupesh Rajendra Chaudhari <141324060+BhupeshChaudhari@users.noreply.github.com>
2024-03-16 14:05:01 +0000

    Stack and Hea[ Memory

--- 01_basics/DataTypesSummary.js
+++ 01_basics/DataTypesSummary.js
@@ -13,3 +13,27 @@
 //     Array -> Object
 //     Object -> Object
 //     Functions -> Function
+
+// ================================================================================================
+
+// Stack (primitive) , Heap (Non-Primitive)
+
+let name1 = "Bhupesh"
+let anotherName = name1
+
+anotherName = "XYZ"
+
+console.log(anotherName);
+console.log(name1);
+
+let userOne = {
+    email : "abc@gmail.com",
+    id : "101",
+}
+
+let userTwo = userOne
+
+userTwo.email = "xyz@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
