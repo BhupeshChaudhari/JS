92fd0634ce7836bc62b6e136742aa89e50b26e33
Bhupesh Rajendra Chaudhari <141324060+BhupeshChaudhari@users.noreply.github.com>
2024-03-17 05:09:58 +0000

    Date and Time

---
+++ 01_basics/07_DateTime.js
@@ -0,0 +1,21 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toISOString());
+
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date("01-14-2024")
+console.log(myCreatedDate.toDateString());
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));
